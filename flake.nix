72cff7a3bcaeb2155a9bf4b79918fc977807d1e8
KangaZero <samuelyongw@gmail.com>
2026-06-01 10:54:14 +0900

    Added more packages needed for dev work

--- flake.nix
+++ flake.nix
@@ -1,26 +1,69 @@
+
 {
-description = "Arch Home Manager";
 
-inputs = {
-  nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  home-manager = {
-    url = "github:nix-community/home-manager";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-};
+  description = "WSL - Arch + Nix Flake by KangaZero";
 
- outputs = { self, nixpkgs, home-manager }:
- let 
-    system = "x86_64-linux";
-    username = "root";
-    hostname = "KangaZero";
-    in
-    {
-	homeConfigurations."${username}" = home-manager.lib.homeManagerConfiguration {
-	pkgs = import nixpkgs { inherit system; };
 
-	modules = [./home.nix];
-	};
+
+  inputs = {
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+
+      url = "github:nix-community/home-manager";
+
+      inputs.nixpkgs.follows = "nixpkgs";
+
     };
+
+#TODO add in nvim config repo when ready
+    # dotfiles-mac = {
+    #
+    #   url = "github:KangaZero/dotfiles-mac";
+    #
+    #   flake = false;
+    #
+    # };
+    #
+  };
+
+
+
+#add in nvim repo inputs when ready
+  outputs = { self, nixpkgs, home-manager}:
+
+    let
+
+      system = "x86_64-linux";
+
+      username = "root";
+
+      hostname = "KangaZero";
+
+    in
+
+    {
+
+      homeConfigurations."${username}" = home-manager.lib.homeManagerConfiguration {
+
+        pkgs = import nixpkgs { inherit system; };
+
+        modules = [
+
+          {
+
+            imports = [ ./home.nix ];
+#add in nvim repo when ready
+            # _module.args.dotfiles-mac = dotfiles-mac;
+
+          }
+
+        ];
+
+      };
+
+    };
+
 }
 
--- home.nix
+++ home.nix
@@ -9,40 +9,60 @@
    git
    zellij
    fzf
+   yazi
    zoxide
    ripgrep
    bat
    eza
    curl
    wget
+   openssh
+   tldr
+# Font
+   nerd-fonts.jetbrains-mono
+# Package managers
+   nodejs_26
+   pnpm
+   rustup
+   python3
+   mise
+   ffmpeg-full
  ];
 
- programs.zsh = { 
+ programs.home-manager.enable = true;
+  programs.zsh = { 
    enable = true;
    enableCompletion = true;
    autosuggestion.enable = true;
+   syntaxHighlighting.enable = true;
 
    oh-my-zsh = {
     enable = true;
     plugins = [
-"git"
-"fzf"
-"colorize"
-"z"
-"history"
-    ];
-   };
-
-  history = {
+	    "git"
+	    "fzf"
+	    "colorize"
+	    "z"
+	    "history"
+    ]; 
+    };
+history = {
    size = 10000;
+   ignoreAllDups = true;
    path = "$HOME/.cache/zsh/history";
   };
 
  };
 
- programs.neovim = {
+programs.neovim = {
  enable = true;
+ defaultEditor = true;
+ #This is needed to use the default .config/nvim/init.lua way to configure nvim
+ sideloadInitLua = true;
  };
+# home.file.".config/nvim" = {
+# source = "${dofiles-mac}/nvim-min";
+#   recursive = true;
+# };
 
- programs.home-manager.enable = true;
 }
